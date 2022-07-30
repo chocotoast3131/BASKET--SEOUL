915b68da4d764193c448c313d530adfd89585bd7
kjy <kjy@naver.com>
2022-07-30 21:19:57 +0900

    jy_commit

--- api/apis.py
+++ api/apis.py
@@ -2,7 +2,7 @@ from django.http import JsonResponse
 from .controller.condition import price_code_name
 
 
-def get_price_code(request):
+def get_price_code(request): # 부류 요청값 받아서 부류별 json데이터로 출력 
     code = request.GET.get("code")
     res_json = price_code_name(code)
     return JsonResponse(res_json)
