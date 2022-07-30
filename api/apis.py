f827917b74589f7911bc810da74dca3ab690bf64
kjy <kjy@naver.com>
2022-07-30 21:17:38 +0900

    jy_commit

--- api/apis.py
+++ api/apis.py
@@ -1,1 +1,8 @@
+from django.http import JsonResponse
+from .controller.condition import price_code_name
 
+
+def get_price_code(request):
+    code = request.GET.get("code")
+    res_json = price_code_name(code)
+    return JsonResponse(res_json)
