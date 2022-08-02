be540d9ef7ff11d89e2116882acebf584aed6e89
anan3351 <rinensi76@naver.com>
2022-08-03 01:25:04 +0900

    global 추가

--- api/controller/condition.py
+++ api/controller/condition.py
@@ -2,7 +2,7 @@ from api_module import f_class__
 from datetime import date, timedelta
 
 def price_code_name(code):
-    
+    global day
     day = date.today() - timedelta(1) #당일 데이터는 없기 때문에 전날을 기준으로 함.
     
     if day.weekday() == 5: #오늘이 토(5), 일(6)이라면 day를 금(4)요일로 만든다
