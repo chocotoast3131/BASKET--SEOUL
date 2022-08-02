b2da4fd375337f77a5659f76d8df7cbc7bddb839
kjy <kjy@naver.com>
2022-08-03 00:22:27 +0900

    jy_commit

--- api/urls.py
+++ api/urls.py
@@ -4,5 +4,5 @@ from . import apis
 
 urlpatterns = [
    # path('get/', apis.get_price_code), # 부류별 json 데이터로 출력
-   # path('getdetail/', apis.get_detail),
+   # path('getdetail/', apis.get_detail), 
 ]
