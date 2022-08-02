74835faa533b1545cace055dbb86d44892be872d
jenshire <chocotoast2001@gmail.com>
2022-08-03 01:00:16 +0900

    #

--- api/controller/price.py
+++ api/controller/price.py
@@ -8,7 +8,7 @@ df = pd.DataFrame(class_func(price_code_name(code)))
 #Detailed_price.py에서 pd.DataFrame을 사용해서 관리하기 편하도록 price도 DateFrame 활용
 
 def price_1(df):
-    return df
+    return df 
 
 print(price_1(df)) #결과 확인용
 #쌀/곡물, 채소, 과일, 수산물 중 필요한 값 입력
