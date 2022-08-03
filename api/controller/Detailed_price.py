6a5301b1ea5eac2d25b3ebcbec7610fe08ff1daf
anan3351 <102586407+anan3351@users.noreply.github.com>
2022-08-03 15:18:48 +0900

    Update Detailed_price.py

--- api/controller/Detailed_price.py
+++ api/controller/Detailed_price.py
@@ -7,18 +7,20 @@ itemcode = input() #품목코드
 kindcode = input()
 day = date.today() - timedelta(1)
 
-df = pd.DataFrame(item_func(f_item__(itemcode, kindcode, day, day))) #식량작물, 채소류, 과일류, 수산물 중 필요한 값을 터미널에 입력
+df = pd.DataFrame(item_func(f_item__(itemcode, kindcode, day, day))) #식량작물, 채소류, 과일류, 수산물 중 필요한 값을 입력
 
 value_name = input()
 index1 = df[df['itemname'] != value_name].index
-df = df.drop(index1) #빈 값의 인덱스를 찾으려 했으나 None, '', "", [], '[]' 등 무엇을 사용해도 빈값을 찾을 수 없어서 품목명을 입력하는 방법을 사용
+df_drop = df.drop(index1) #빈 값의 인덱스를 찾으려 했으나 None, '', "", [], '[]' 등 무엇을 사용해도 빈값을 찾을 수 없어서 품목명을 입력하는 방법을 사용
 #{'itemname': [], 'kindname': [], 'marketname': [], 'today': '07/25', 'price': '48,943'} <- 이와 같은 빈 항목들 제거
 #drop을 사용하기 위해 pandas 사용 / df를 데이터프레임으로 만들었음
 
-def Detailed(df1):
-    return df1
+df_json = df_drop.to_json(orient = 'records') 
 
-print(Detailed(df)) #결과 확인용
+def Detailed(df_json):
+    return df_json
 
-#품목코드, 품종코드 입력. 날짜는 일주일 간격으로 출력됨
+print(pd.read_json(Detailed(df_json))) #결과 확인용/json파일을 데이터프레임으로 출력함
+
+#품목코드, 품종코드 입력
 #ex) 111, 01, 쌀
