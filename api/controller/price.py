45326fc556ed72b55468dd6d0220e57d21ea3c4b
anan3351 <102586407+anan3351@users.noreply.github.com>
2022-08-03 15:18:28 +0900

    Update price.py

--- api/controller/price.py
+++ api/controller/price.py
@@ -8,11 +8,14 @@ code = input()
 
 df = pd.DataFrame(class_func(price_code_name(code)))
 
-df1 = df.drop_duplicates(['item_name']) #메인 키워드(itemname) 하나만 남기고 중복값 제거(상품/중품 등)
+df_drop = df.drop_duplicates(['item_name']) #메인 키워드(itemname) 하나만 남기고 중복값 제거(상품/중품 등)
+# df_json = df1.to_json(orient = 'index')
+# df_json = pd.DataFrame.to_json(df1)
+df_json = df_drop.to_json(orient = 'records')
 
-def price_1(df1):
-    return df1
+def price_1(df_json):
+    return df_json
 
-print(price_1(df1)) #결과 확인용
+print(pd.read_json(price_1(df_json))) #결과 확인용/json파일을 데이터프레임으로 출력함
 #쌀/잡곡, 채소, 과일, 수산물 중 필요한 값 입력
 #ex)쌀/잡곡
