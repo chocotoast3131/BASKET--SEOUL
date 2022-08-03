a7335e025d3f02b3155acf8ebbee811c1f20533a
jenshire <kjy@naver.com>
2022-08-03 18:51:51 +0900

    jy_commit

--- api/apis.py
+++ api/apis.py
@@ -5,7 +5,7 @@ from api.controller.price import price_1
 def get_price_code(request): # 부류 요청값 받아서 부류별 json데이터로 출력 
     name = request.GET.get("name")
     res_json = price_1(name)
-    return JsonResponse(res_json)
+    return JsonResponse(res_json, safe=False)
 
 # def get_detail_code(request):
 #     res_json = Detailed()
