7007e691d0a83ca4d2397c47ee2b8b69a5aa5051
anan3351 <rinensi76@naver.com>
2022-08-04 22:59:12 +0900

    데이터 추출/상세정보

--- api/controller/api_module.py
+++ api/controller/api_module.py
@@ -30,13 +30,29 @@ def class_func(res_json): # 부류별
     for class_ in res_json['data']['item']: #사용할 key만 가져옴
         res_class_ = { \
             "item_name": class_["item_name"], \
-            # "kind_name": class_["kind_name"], \
             "unit": class_["unit"], \
-            "today": class_["dpr1"], \
             "past_month": class_["dpr5"], \
+            "today": class_["dpr1"] \
         }
         res_class_f.append(res_class_)
     return res_class_f
+
+def class_func1(res_json): # 부류별
+    res_class_f1 = list() #새로운 dict 생성 dict() 대신 list로 캐스팅
+    if type(res_json['data']) is list:
+        if res_json['data'][0] == "001": 
+            print('no data')
+            return res_class_f1
+        
+    for class_1 in res_json['data']['item']: #사용할 key만 가져옴
+        res_class_1 = { \
+            "item_name": class_1["item_name"], \
+            "kind_name": class_1["kind_name"], \
+            "past_month": class_1["dpr5"], \
+            "today": class_1["dpr1"] \
+        }
+        res_class_f1.append(res_class_1)
+    return res_class_f1
 
 def item_func(res_json): # 품목별
     res_item_f = list()
@@ -56,6 +72,24 @@ def item_func(res_json): # 품목별
         res_item_f.append(res_item_)
     return res_item_f
 
+def graph_func(res_json): # 품목별
+    res_graph_f = list()
+    if type(res_json['data']) is list:
+        if res_json['data'][0] == "001":
+            print('no data')
+            return res_graph_f
+        
+    for graph_ in res_json['data']['item']:
+        res_graph_ = { \
+            "itemname": graph_["itemname"], \
+            "kindname": graph_["kindname"], \
+            "marketname": graph_["marketname"], \
+            "today": graph_["regday"], \
+            "price": graph_["price"]
+        }
+        res_graph_f.append(res_graph_)
+    return res_graph_f
+
 def f_class__(p_item_category_code, p_regday, p_product_cls_code='01', p_country_code='1101'):
     return open_api(p_regday=p_regday, p_item_category_code=p_item_category_code, \
         p_product_cls_code=p_product_cls_code, p_country_code=p_country_code, \
@@ -65,4 +99,3 @@ def f_item__(p_itemcode, p_kindcode, p_startday, p_endday, p_productclscode='01'
     return open_api(p_itemcode=p_itemcode, p_startday=p_startday, p_endday=p_endday, p_kindcode=p_kindcode, \
         p_productclscode=p_productclscode, p_countrycode=p_countrycode, p_productrankcode=p_productrankcode, \
         action='periodProductList')
-        
