b0df3c8a8f8a6edc0e79bcf5c63f20ba7caf76e5
Goun Park <gounpark7475@gmail.com>
2022-08-06 03:02:48 +0900

    front-end0806

--- basket/static/javascript/detailsinformation.js
+++ basket/static/javascript/detailsinformation.js
@@ -1,23 +1,11 @@
 //fetch('url')
 //.then()은 서버와 통신 후 response 객체 return
-function getTable(tab) {
-  const category_code_map = {
-    tab01: "과일",
-    tab02: "채소",
-    tab03: "쌀/잡곡",
-    tab04: "수산물",
-  };
+$(function() {
+  var urls = $(location).attr('pathname').split('/');
 
-  const paramsString =
-    "name="+category_code_map[tab];
-
-
-  const searchParams = new URLSearchParams(paramsString);
-
-
-  $.ajax("http://127.0.0.1:8000/api/get/", {
+  $.ajax({
+    url:"http://127.0.0.1:8000/api/test/" + urls[2] + "/" + urls[3],
     type: "GET", // HTTP method type(GET, POST) 형식.
-    data: searchParams.toString(), // Json 형식의 데이터.
     dataType: "json",
   }).done(function (json) {
     let items = json;
@@ -26,14 +14,14 @@ function getTable(tab) {
       .map(
         (item) => `<tr>
                     <td>${item.item_name}</td>
-                    <td>${item.unit}</td>
-                    <td>${item.past_month}</td>
+                    <td>${item.kind_name}</td>
+                    <td>${item.day1}</td>
                     <td>${item.today}</td>
+                    <td>${item.past_month}</td>
                   </tr>`
       )
       .reduce((prev, curr) => prev + curr);
 
     $("#table tbody").html(table_body);
   });
-}
-
+});
