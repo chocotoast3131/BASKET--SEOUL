a7eb2103d9226695c2cfb6d67280b1853260bd03
jenshire <chocotoast2001@gmail.com>
2022-08-07 03:07:22 +0900

    modify

--- api/apis.py
+++ api/apis.py
@@ -1,6 +1,5 @@
 from django.http import JsonResponse
 from api.controller.Detailed_price import Detailed, Detailed_graph
-
 from api.controller.price import price_1
 
 def get_price_code(request): # 부류 요청값 받아서 부류별 json데이터 
