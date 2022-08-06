5524a3df8db585b2d1b2d92186b64b0e47694eb8
jenshire <kjy@naver.com>
2022-08-06 20:38:34 +0900

    #

--- basket/static/javascript/detailsinformation.js
+++ basket/static/javascript/detailsinformation.js
@@ -1,56 +1,27 @@
 //fetch('url')
 //.then()은 서버와 통신 후 response 객체 return
-function getTable(tab) {
-    const category_code_map = {
-        tab01: "#",
-        tab02: "#",
-        tab03: "#",
-        tab04: "#",
-        tab05: "#",
-        tab06: "#",
-        tab07: "#",
-        tab08: "#",
-        tab09: "#",
-        tab10: "#",
-        tab11: "#",
-        tab12: "#",
-        tab13: "#",
-        tab14: "#",
-        tab15: "#",
-        tab16: "#",
-        tab17: "#",
-        tab18: "#",
-        tab19: "#",
-        tab20: "#",
-        tab20: "#"
-    };
-
-    const paramsString =
-        "name=" + category_code_map[tab];
-
-
-    const searchParams = new URLSearchParams(paramsString);
-
-
-    $.ajax("###", {
-        type: "GET", // HTTP method type(GET, POST) 형식.
-        data: searchParams.toString(), // Json 형식의 데이터.
-        dataType: "json",
+$(function() {
+    var urls = $(location).attr('pathname').split('/');
+  
+    $.ajax({
+      url:"http://127.0.0.1:8000/api/test/" + urls[2] + "/" + urls[3],
+      type: "GET", // HTTP method type(GET, POST) 형식.
+      dataType: "json",
     }).done(function (json) {
-        let items = json;
-
-        let table_body = items
-            .map(
-                (item) => `<tr>
-                            <td>${item.item_name}</td>
-                            <td>${item.kind_name}</td>
-                            <td>${item.unit}</td>
-                            <td>${item.today}</td>
-                            <td>${item.past_month}</td>
-                            </tr>`
-            )
-            .reduce((prev, curr) => prev + curr);
-
-        $("#table tbody").html(table_body);
+      let items = json;
+  
+      let table_body = items
+        .map(
+          (item) => `<tr>
+                      <td>${item.item_name}</td>
+                      <td>${item.kind_name}</td>
+                      <td>${item.day1}</td>
+                      <td>${item.today}</td>
+                      <td>${item.past_month}</td>
+                    </tr>`
+        )
+        .reduce((prev, curr) => prev + curr);
+  
+      $("#table tbody").html(table_body);
     });
-}
+  });
