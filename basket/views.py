f5dcbe6b88695be86a463097c888c1f500d534ce
jenshire <kjy@naver.com>
2022-08-09 17:51:57 +0900

    .

--- basket/views.py
+++ basket/views.py
@@ -47,37 +47,3 @@ def searched(request):
 def detailed(request,category,itemname):
     return render(request, 'basket/detailed.html', {"category":category,"itemname":itemname})
 
-
-
-
-# class ResultsView(ListView): # 검색결과를 위한 클래스
-#     template_name = "basket/search.html"
-#     # qs = ItemName.objects.all()
-#     context_object_name = 'result'
-#     model = ItemName
-#     page_kwarg = 'name' # 쿼리스트링 페이지 키워드
-
-#     def get_context_data(self, **kwargs):
-#         context =  super().get_context_data(**kwargs)
-#         context["name"] = context
-#         return context
-
-
-#     # count = 0 # 검색결과 갯수
-# #     def __init__(self,name):
-# #         self.name = name
-
-# #     def result_count():
-# #         qs = ItemName.objects.all()
-# #         return qs.count()
-
-# # # 검색
-# #     def search(request):
-# #         return render(request, 'basket/search.html')
-
-#     # def searched(request): # 검색결과
-#     #     name = request.GET.get("name") # 쿼리스트링 받기
-#     #     results = ItemName.objects.filter(Q(category_name__icontains = name)|Q(item_name__icontains = name)|Q(kind_name__icontains = name)) # Q 조건문, name이 ~이거나 ~인것
-#     #     return render(request, 'basket/search.html', {"results":results, "name":name})
-
-# # print (search.result_count())
