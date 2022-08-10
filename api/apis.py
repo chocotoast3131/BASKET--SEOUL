d6679f9bb69db858edbbc4eafc397c15978f0cc7
anan3351 <rinensi76@naver.com>
2022-08-11 03:44:01 +0900

    수정

--- api/apis.py
+++ api/apis.py
@@ -12,9 +12,9 @@ def get_detail_code(request, name, kindname): # 상세정보 json 데이터
     res_json = Detailed(name, kindname)
     return JsonResponse(res_json,safe=False)
 
-def get_detail_graph(request, value_name): # 상세정보 그래프 json 데이터
-    res_json = Detailed_graph(value_name)
-    return JsonResponse(res_json, safe=False)
+# def get_detail_graph(request, value_name): # 상세정보 그래프 json 데이터
+#     res_json = Detailed_graph(value_name)
+#     return JsonResponse(res_json, safe=False)
 
 def get_fish_graph(request, value_name): # 수산물 그래프 json 데이터
     res_json = marine_products_graph(value_name)
