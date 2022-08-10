a1c32ea97d3f7a04e62037782fb63a79f36dc78d
anan3351 <rinensi76@naver.com>
2022-08-11 03:44:01 +0900

    수정

--- api/controller/Detailed_price.py
+++ api/controller/Detailed_price.py
@@ -14,7 +14,7 @@ def Detailed(name, kindname): #품종별로도 볼수있어야함
     df_json = df_drop.to_json(orient = 'records')
     df_dict = json.loads(df_json)
     return df_dict
-# print(Detailed("수산물", "갈치")) #확인용 #kindname에 있는데 출력되지 않는 데이터는 없는 데이터. 존재하는 데이터만 나옴
+# print(Detailed("과일", "사과")) #확인용 #kindname에 있는데 출력되지 않는 데이터는 없는 데이터. 존재하는 데이터만 나옴
 
 
 def Detailed_graph(value_name): #마켓명 하나만 출력할 수 있음/그래프용 데이터
@@ -39,13 +39,13 @@ def Detailed_graph(value_name): #마켓명 하나만 출력할 수 있음/그래
     plt.plot(months, price, color='#ff7f0e')
     plt.xlabel('날짜', loc='right')
     plt.ylabel('가격', loc='top', rotation=360)
-    plt.savefig('basket/static/logo,img/graph.png')
+    plt.savefig('basket/static/logo,img/graph.png', transparent=True)
     
     graph_img = Image.open('basket/static/logo,img/graph.png')
-
+    
     return graph_img
 
-# print(Detailed_graph('레몬'))
+# print(Detailed_graph('물오징어'))
 
 def marine_products_graph(value_name): #수산물 그래프용 함수
     
